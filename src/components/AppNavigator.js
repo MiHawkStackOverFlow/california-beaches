afad6e7a9926dada9a973948f9f400081a9ab53e
Abhishek Sharma <abhivsharma8@gmail.com>
2023-02-24 21:36:00 +0530

    setting up the react router

---
+++ src/components/AppNavigator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppNavigator() {
+  return (
+      <div>
+        <h2>Test</h2>
+      </div>
+  )
+}
---
+++ src/containers/Beaches.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Beaches() {
+  return (
+    <div>
+        <h1>California Beaches !!!</h1>
+    </div>
+  )
+}
