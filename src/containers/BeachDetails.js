2a57c3ee34bdd2df30504481b28a3faa51bc4331
Abhishek Sharma <abhivsharma8@gmail.com>
2023-02-26 12:12:45 +0530

    added box to show beach details

--- src/containers/BeachDetails.js
+++ src/containers/BeachDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import { CALIFORNIA_BEACHES_API_URL } from '../config';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
 export default class BeachDetails extends Component {
   constructor(props) {
@@ -16,16 +17,27 @@ export default class BeachDetails extends Component {
     axios.get(CALIFORNIA_BEACHES_API_URL + '/id/' + id).then((response) => {
       if(response.status >= 200 && response.status < 300) {
         console.log(response.data); 
-        this.setState({ beach: response.data })
+        this.setState({ beach: response.data[0] })
       }
     })
   }
 
   render() {
-    return (
-      <div style={{ marginTop: 50 }}>
-        <h1>Hello</h1>
-      </div>
-    );
+    const { beach } = this.state;
+    if (beach) {
+      const name = beach.NameMobileWeb;
+      return (
+        <Box>
+          <Box sx={{ height: '84vh', backgroundColor: 'black',  color: 'white', marginTop: 10, textAlign: 'center', borderRadius: 5,
+                     paddingTop: 3 }}>
+             <Typography sx={{ textTransform: 'upperCase', fontFamily: 'fantasy' }} variant='h1'>
+               { name }
+             </Typography>
+          </Box>
+        </Box>
+      )
+    } else {
+      return <CircularProgress />
+    }
   }
 }
