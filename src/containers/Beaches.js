3c3e4af3f5d002d7878bcc7b13e7bafafd17b3e6
Abhishek Sharma <abhivsharma8@gmail.com>
2023-02-28 13:07:52 +0530

    search functionality added

--- src/containers/Beaches.js
+++ src/containers/Beaches.js
@@ -3,13 +3,29 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import BeachCard from "../components/BeachCard";
 import { CALIFORNIA_BEACHES_API_URL } from '../config';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const checkURL = (url) => {
   return(url.toLowerCase().match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xxs: 0, // small phone
+      xs: 300, // phone
+      sm: 600, // tablets
+      md: 900, // small laptop
+      lg: 1200, // desktop
+      xl: 1536 // large screens
+    }
+  }
+});
+
+
 export default function Beaches() {
   const [beachData, setBeachData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     axios.get(CALIFORNIA_BEACHES_API_URL).then((response) => { 
       if(response.status >= 200 && response.status < 300) {
@@ -25,7 +41,8 @@ export default function Beaches() {
                                                                       : null)))),
             name: beach.NameMobileWeb,
             lat: beach.LATITUDE,
-            lng: beach.LONGITUDE 
+            lng: beach.LONGITUDE,
+            county: beach.COUNTY 
           };
           newBeachData.push(beachObject);   
         });
@@ -36,13 +53,27 @@ export default function Beaches() {
   return (
     <Box>
        { beachData ? (
-          <Grid style={{ backgroundColor: 'rgb(68,68,68)', textAlign: 'center', padding: '70px 10px 0px 10px' }} container spacing={2}>
-            { beachData.map((beach) => { 
-              return (
-                <BeachCard key={beach.id} beach={beach} image={beach.image}/>
-              )  
-            })}
-          </Grid>
+         <ThemeProvider theme={theme}>
+            <Box  style={{ height: '100%', width: '100%', backgroundColor: 'rgb(68,68,68)', textAlign: 'center' }}>
+              <input style={{ width: '70%', marginTop: '70px', padding: '10px' }} type="text" placeholder="Search For a Beach by name or county" 
+                    onChange={ (event) => { setSearchTerm(event.target.value) }} />
+              <Grid style={{ textAlign: 'center', padding: '40px 10px 10px 10px' }} container spacing={2}>
+                { beachData.filter((beach) => {
+                  if(searchTerm === "") {
+                    return beach;
+                  } else if (beach.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return beach;
+                  } else if (beach.county.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return beach;
+                  }
+                }).map((beach) => { 
+                  return (
+                    <BeachCard key={beach.id} beach={beach} image={beach.image}/>
+                  )  
+                })}
+              </Grid>
+            </Box>
+         </ThemeProvider>
        ) : (<CircularProgress style={{ marginTop: 100, marginLeft: 150 }} /> )}
     </Box>
   )
